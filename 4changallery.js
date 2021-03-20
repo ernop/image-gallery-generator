caa4610335acc2756f84d56f8ad3810440e6b230
Ernie <ernestfrench@gmail.com>
2021-03-20 13:19:57 -0700

    webm support

--- 4changallery.js
+++ 4changallery.js
@@ -1,15 +1,17 @@
 $(function() {
-	var imageCount = 0;
-	var imagePosition = 0;
+	//the human uploaded images.
+	var originalImageNames = [];
 	
 	//imageUrls
 	var images = [];
 	
-	//the human uploaded images.
-	var originalImageNames = [];
-    console.log("format");
+	//for storing which ones are webms
+	var imageTypes = [];
 	
-	// will be loaded from local storage.
+	//current position in playback.
+	var imagePosition = 0;
+	
+	// settings, loaded from local storage
 	var imageCountShown = true;
 	var imageFilenameShown = true;
 	var imageResolutionShown = false;
@@ -22,30 +24,39 @@ $(function() {
 		  console.log(`Error: ${error}`);
 		}
 		
-		
+		//save settings
 		function apply(item) {
 		  imageCountShown = item.data.showCount;
 		  imageFilenameShown = item.data.showFilename;
 		  imageResolutionShown = item.data.showResolution;
 		  imageMegapixelsShown = item.data.showMegapixels;
-		  console.log("imageCountShown"+imageCountShown);
-		  console.log("imageFilenameShown"+imageFilenameShown);
-		  console.log("imageResolutionShown"+imageResolutionShown);
-		  console.log("imageMegapixelsShown"+imageMegapixelsShown);
 		}
 
 		let getting = browser.storage.sync.get("data");
 		getting.then(apply, onError);
 	}
+	
+	function getFileType(path){
+		var res = "";
+		if (path.substr(path.length - 5) == ".webm"){
+			res = "video";
+		}
+		else{
+			res = "image";
+		}
+		return res;
+		console.log("getting filetype for"+path+"=="+res);
+	}
 
 	function setup(){
-		console.log("1");
-		
 		loadStoredSettings();
 		
 		$(".fileText").each(function(index) {
 			var path = $(this).find("a").attr("href").substring(2);
 			images.push(path);
+			imageTypes[index] = getFileType(path);
+			
+			//find image name from the original upload - sometimes contains useful/interesting information
 			var originalImageName = $(this).find("a").attr("title");
 			
 			//fallback to a.text
@@ -54,23 +65,20 @@ $(function() {
 			}
 			originalImageNames.push(originalImageName);
 		});
-		console.log("2");
+		
+		//appears not to work right now.
 		setupFirstPreloads();
-		console.log("3");
-		imageCount = images.length;
 
 		$('.navLinks').prepend('[<a href="#" class="galleryOn">Gallery Mode</a>] ');
 		$('body').wrapInner("<div class='oldBody'></div>");
 		
 		//gallery load
-		console.log("4");
 		$(".galleryOn").click(function() {
 			//new document structure body > oldBody => body > (galleryViewWrapper)(oldBody)
 			$(".oldBody").hide();
-			console.log("5");
 			$("body").css("padding","0");
 			if ($("#galleryViewWrapper").length==0){
-				$('body').prepend('<div id="galleryViewWrapper"><div id="labelZone" style="color:white;float:left;position:absolute;z-index:202;padding:5px;"></div><div id="blackBackground" style="z-index:100;position:absolute;display:flex;align-items:center;justify-content: left:0;top:0;display:none;width:100%;height:100%;background-color:black;position:absolute;z-index:200;"><img id="targetImg" style="max-width:99%;max-height:99%;" src=""/><img id="targetImg_preload1" style="display:none;" /><img id="targetImg_preload2" style="display:none;"/><img id="targetImg_preload3" style="display:none;"/><img id="targetImg_preload4" style="display:none;"/></div></div>');
+				$('body').prepend('<div id="galleryViewWrapper"><div id="labelZone" style="color:white;float:left;position:absolute;z-index:202;padding:5px;"></div><div id="blackBackground" style="z-index:100;position:absolute;display:flex;align-items:center;justify-content: left:0;top:0;display:none;width:100%;height:100%;background-color:black;position:absolute;z-index:200;"><img id="targetImg" style="max-width:99%;max-height:99%;display:none;" src=""/><video controls="true" autoplay="" id="targetVideo" style="max-width:99%;max-height:99%;display:none;" src=""/><img id="targetImg_preload1" style="display:none;" /><img id="targetImg_preload2" style="display:none;"/><img id="targetImg_preload3" style="display:none;"/><img id="targetImg_preload4" style="display:none;"/></div></div>');
 			}
 			$("#galleryViewWrapper").show();
 			$("#blackBackground").show();
@@ -127,7 +135,7 @@ $(function() {
 	
 	function imageCountLabel(){
 		if (imageCountShown){
-			$("#labelZone").append('<div id="imageCount">' + imagePosition + "/" + (imageCount-1)+ '</div>');
+			$("#labelZone").append('<div id="imageCount">' + imagePosition + "/" + (images.length-1)+ '</div>');
 		}
 		else{
 			$("#imageCount").remove();
@@ -175,19 +183,31 @@ $(function() {
 	
 	function reDraw() {
 		imagePosition=Math.max(0, imagePosition);
-		imagePosition=Math.min(imagePosition, imageCount-1);
-		document.getElementById("targetImg").src = "http://"+images[imagePosition];
-		redrawLabel();
-		$("#labelZone").append("<span class='dots'> ...</span>");
-		document.getElementById("targetImg_preload1").src = "http://"+images[imagePosition+1];
-		document.getElementById("targetImg_preload2").src = "http://"+images[imagePosition+2];
-		document.getElementById("targetImg_preload3").src = "http://"+images[imagePosition+3];
-		document.getElementById("targetImg_preload4").src = "http://"+images[imagePosition+4];
-		//annoyance: if preloading is done, then transition is instant.
-		//but if it's not done, then keyboard actions have no visible result until image is loaded.
-		$("#targetImg").one("load",function(){
-			$(".dots").remove();
-		});
+		imagePosition=Math.min(imagePosition, images.length-1);
+		var thisImageType = imageTypes[imagePosition];
+		console.log(thisImageType);
+		if (thisImageType=="video"){ //webms
+			$("#targetImg").hide();
+			$("#targetVideo").show();
+			document.getElementById("targetVideo").src = "//"+images[imagePosition];
+		}
+		else{ //normal img tag can display these
+			$("#targetVideo").hide();
+			$("#targetImg").show();
+			
+			document.getElementById("targetImg").src = "//"+images[imagePosition];
+			redrawLabel();
+			$("#labelZone").append("<span class='dots'> ...</span>");
+			document.getElementById("targetImg_preload1").src = "http://"+images[imagePosition+1];
+			document.getElementById("targetImg_preload2").src = "http://"+images[imagePosition+2];
+			document.getElementById("targetImg_preload3").src = "http://"+images[imagePosition+3];
+			document.getElementById("targetImg_preload4").src = "http://"+images[imagePosition+4];
+			//annoyance: if preloading is done, then transition is instant.
+			//but if it's not done, then keyboard actions have no visible result until image is loaded.
+			$("#targetImg").one("load",function(){
+				$(".dots").remove();
+			});
+		}
 	}
 	
 	function setKeyboardShortcuts(){
@@ -201,7 +221,7 @@ $(function() {
 					imagePosition+=1;
 					break;
 				case 35: // end
-					imagePosition=imageCount-1;
+					imagePosition=images.length-1;
 					break;
 				case 36: // home
 					imagePosition=0;
